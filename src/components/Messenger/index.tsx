58a60eb686aae3f6d5d7aedd4793f671dc8455b2
Crucial <alisxan006@mail.ru>
2025-02-11 18:14:54 +0300

    fix

--- src/components/Messenger/index.tsx
+++ src/components/Messenger/index.tsx
@@ -10,6 +10,21 @@ export const Messenger = () => {
         <h5>Сопроводительное письмо</h5>
         <p>Меня зовут Сергей и я автомаляр с 4-летним опытом.</p>
       </div>
+      <div className="messenger__date">
+        <span>20 июля</span>
+      </div>
+      <div className="messenger__message">
+        <span className="messenger__message-name">Ирина Корчагина</span>
+        <h4 className="messenger__message-type">Приглашение</h4>
+        <p className="messenger__message-text">
+          Добрый день, Сергей! Нас заинтересовал Ваш отклик. Свяжитесь с нами
+          по номеру +7 (999) 999-99-99 или оставьте свой номер и мы Вам
+          перезвоним.
+        </p>
+        <p className="messenger__message-ps">
+          С уважением, Ирина, специалист по привлечению персонала
+        </p>
+      </div>
     </div>
   );
 };
